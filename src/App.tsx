e1b77ea2dfd8209a2a47ff81ea96801350ac237e
Enk0rt <delovoyartem4ick@gmail.com>
2025-01-12 20:42:32 +0200

    update - main branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
-import {FC} from 'react';
 
-type MyPropsType = {text: string};
 
-const App:FC<MyPropsType> = ({text}) => {
+function App () {
     return (
         <div>
-            {text}
         </div>
     );
-};
+}
 
 export default App;
