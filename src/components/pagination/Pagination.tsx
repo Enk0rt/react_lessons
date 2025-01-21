94f4e0109523ad9f6b0da5a2a7351838947cb9b9
Enk0rt <delovoyartem4ick@gmail.com>
2025-01-21 19:03:44 +0200

    Update - Homework - Lesson4

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,32 +1,24 @@
 import {useSearchParams} from "react-router-dom";
 import {FC} from "react";
 
-type Props={
-    skip:number,
-    total:number,
+type Props = {
+    skip: number,
+    total: number,
 }
 
-export const Pagination:FC<Props> = ({skip,total}) => {
-
+export const Pagination: FC<Props> = ({skip, total}) => {
     const [query, setQuery] = useSearchParams({limit: '10', skip: '0'});
+
+    const amountHandler = (delta:number) => {
+        const skip:number = Number(query.get('skip'))
+        setQuery({skip: (skip + delta).toString()})
+    }
+
     return (
         <div className={'flex justify-center gap-4 mt-6'}>
-            <button className={'border border-black p-2 px-4 hover:bg-white hover:bg-opacity-50'} onClick={
-                () => {
-                    const skip: number = Number(query.get('skip'))
-                    setQuery({skip: (skip - 10).toString()})
-
-                }
-            } disabled={skip===0}>prev
+            <button className={'border border-black p-2 px-4 hover:bg-white hover:bg-opacity-50'} onClick={() => amountHandler(-10)} disabled={skip === 0}>prev
             </button>
-            <button className={'border border-black p-2 px-4 hover:bg-white hover:bg-opacity-50'} onClick={
-                () => {
-                    const skip: number = Number(query.get('skip'))
-                    setQuery({skip: (skip + 10).toString()})
-                    console.log(total)
-                }
-            }
-                    disabled={skip > total-10}>next
+            <button className={'border border-black p-2 px-4 hover:bg-white hover:bg-opacity-50'} onClick={() => amountHandler(10)} disabled={skip > total - 10}>next
             </button>
         </div>
     );
